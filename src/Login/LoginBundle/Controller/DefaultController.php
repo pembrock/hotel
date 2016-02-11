48d457ffa03c1c20cd127dc9191dc2583b62e7cd
Kirill <pembrock@gmail.com>
2016-02-11 15:29:40 +0300

    fxd

--- src/Login/LoginBundle/Controller/DefaultController.php
+++ src/Login/LoginBundle/Controller/DefaultController.php
@@ -4,21 +4,25 @@ namespace Login\LoginBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+use Login\LoginBundle\Entity\Users;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        $login = "test";
-        $password = "test";
-        $em = $this->getDoctrine()->getManager();
-        $repository = $em->getRepository('LoginLoginBundle:Users');
-        $user = $repository->findOneBy(array('login' => $login, 'password' => $password));
-        if ($user) {
-            return $this->render('LoginLoginBundle:Pages:login.html.twig', array('name' => $user->getName()));
-        }
-        else{
-            return $this->render('LoginLoginBundle:Pages:index.html.twig', array('name' => "Login failed"));
+class DefaultController extends Controller {
+
+    public function indexAction(Request $request) {
+        if ($request->getMethod() == "POST") {
+            $login = $request->get('login');
+            $password = $request->get('password');
+
+            $em = $this->getDoctrine()->getManager();
+            $repository = $em->getRepository('LoginLoginBundle:Users');
+            $user = $repository->findOneBy(array('login' => $login, 'password' => $password));
+            if ($user) {
+                return $this->render('LoginLoginBundle:Pages:login.html.twig', array('name' => $user->getName()));
+            }
+        } else {
+            return $this->render('LoginLoginBundle:Pages:login.html.twig');
         }
     }
+
 }
